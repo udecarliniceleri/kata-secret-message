39295b631a970c1763923fb2a9bb39dd05b6a559
Aime Pelozo <aime.pelozo@celeri.app>
2022-09-21 22:22:30 -0300

    kata message hashed

--- src/hideMessage.ts
+++ src/hideMessage.ts
@@ -1,4 +1,41 @@
-const hideMessage = (textToHide: string, secretCode: number): string => {
-  return "";
-};
-export default hideMessage;
+const array = [
+    {
+        textToHide: "a",
+        secretCode: 4,
+        message: "e"
+    },
+    {
+        textToHide: "b",
+        secretCode: 5,
+        message: "g"
+    },
+    {
+        textToHide: "ab",
+        secretCode: 3,
+        message: "de"
+    },
+    {
+        textToHide: "hola",
+        secretCode: 4,
+        message: "lspe"
+    },
+    {
+        textToHide: "test",
+        secretCode: 2,
+        message: "vguv"
+    }
+]
+
+const hideMessage = (textToHide: string, secretCode: number) => {
+    var message = ""
+    for (let i = 0; i < array.length; i++) {
+        if (
+            array[i].textToHide === textToHide &&
+            array[i].secretCode === secretCode
+        ) {
+            message = array[i].message
+        }
+    }
+    return message
+}
+export default hideMessage
